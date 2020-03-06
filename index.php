9d20850102cfc7f97b4b8820b7d7b89b5fe7048f
medicare <mcareducation@gmail.com>
2020-03-06 10:00:23 +0000

    new section

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="zxx">
+
 <head>
 	<title>Kelungkung Tournament | ML & PUBG</title>
 	<meta charset="UTF-8">
 	<meta name="description" content="Registrasi Pendafataran Tournament Mobile Legend dan PUBG, Remaja Lokok Kelungkung">
 	<meta name="keywords" content="bayan, tournament, mobile, legends, pubg, lokok, kelungkung">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Favicon -->   
-	<link href="img/ml-pubg/tournament.png" rel="shortcut icon"/>
+	<!-- Favicon -->
+	<link href="img/ml-pubg/tournament.png" rel="shortcut icon" />
 
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i" rel="stylesheet">
 
 	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="css/font-awesome.min.css"/>
-	<link rel="stylesheet" href="css/owl.carousel.css"/>
-	<link rel="stylesheet" href="css/style.css"/>
-	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="css/animate.css" />
 
 
-	
+
 
 </head>
+
 <body>
 	<!-- Page Preloder -->
 	<!-- <div id="preloder">
@@ -50,72 +53,52 @@
 		</div>
 	</header>
 	<!-- Header section end -->
-	<!-- Latest news section -->
-	
-	<!-- Latest news section end -->
-	
-
-	<!-- Hero section -->
-	<!-- <section class="hero-section">
-		<div class="hero-slider owl-carousel">
-			
-			<div class="hs-item set-bg" data-setbg="img/ml-pubg/ml1.jpg">
-				<div class="hs-text">
-					<div class="container">
-						<h2>Tournament<span> Mobile Legends</span> of Bayan </h2>
-						<p>Tournament Mobile Legends di selanggarakan Atas dasar Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, modi.</p>
-						<a href="#" class="site-btn">Terms and Conditions</a>
-					</div>
-				</div>
-			</div>
-			<div class="hs-item set-bg" data-setbg="img/ml-pubg/pubg.jpg">
-				<div class="hs-text">
-					<div class="container">
-						<h2>Tournament<span> Pubg Mobile</span> of Bayan</h2>
-						<p>Tournament Pubg Mobile di selanggarakan Atas dasar Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, modi.</p>
-						<a href="#" class="site-btn">Terms and Conditions</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section> -->
-	<!-- Hero section end -->
-
 	<section class="page-info-section set-bg mb-0" data-setbg="img/ml-pubg/bgfull.png" style="background-image: url(&quot;img/&quot;);">
 	</section>
 	<div class="latest-news-section mb-0">
 		<div class="ln-title">Important Informations</div>
 		<div class="news-ticker">
 			<div class="news-ticker-contant">
-				<div class="nt-item"><span class="new">Deadline Register</span> 30 march 2020  </div>
-				<div class="nt-item"><span class="racing" >Play on </span> 08:00 Am, 3 April 2020 at Warball Cafe   </div>
-				<div class="nt-item"><span class="strategy" >Contact WA </span> +6287 8656 4444  </div>
+				<div class="nt-item"><span class="new">Deadline Register</span> 30 march 2020 </div>
+				<div class="nt-item"><span class="racing">Play on </span> 08:00 Am, 3 April 2020 at Warball Cafe </div>
+				<div class="nt-item"><span class="strategy">Contact WA </span> +6287 8656 4444 </div>
 			</div>
 		</div>
 	</div>
 	<!-- Feature section -->
 	<section class="feature-section spad mt-0">
+		<div class="row justify-content-center mb-0">
+			<div class="col-12 col-md-10">
+				<div class="section-heading text-center">
+					<p>REGISTRASI</p>
+					<h4>REGISTRASI TOURNAMNENT
+					</h4>
+				</div>
+			</div>
+		</div>
 		<div class="container">
 			<div class="row">
+
 				<div class="col-lg-6 col-md-6 p-0">
 					<div class="feature-item set-bg" data-setbg="img/ml-pubg/ml2.jpg">
-						<span class="cata new">REGISTER FOR MOBILE LEGENDS</span>
+						<span class="cata new">MOBILE LEGENDS</span>
+
 						<div class="fi-content text-white">
 							<h5>Tournament Mobile Legends of Bayan</h5>
 							<?php
-								$dateTime=date("d-m-Y");
-								
+							$dateTime = date("d-m-Y");
 
-								if ($dateTime=="30-03-2020") {
-									?>
-										<button class="btn btn-disabled">Pendaftaran Ditutup</button>
-									<?php
-								}
-								else{
-									?>
-										<a class="site-btn2" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeFQKkX2hk6Q7-QDKlkvYYVWHBbpixeWNSA9aHxJ65TGjR__w/viewform">CLICK TO REGISTER</a>
-									<?php
-								}
+
+							if ($dateTime == "30-03-2020") {
+							?>
+								<button class="btn btn-disabled">Pendaftaran Ditutup</button>
+							<?php
+							} else {
+							?>
+								<a href="https://docs.google.com/forms/d/e/1FAIpQLSeFQKkX2hk6Q7-QDKlkvYYVWHBbpixeWNSA9aHxJ65TGjR__w/viewform" class="regis-ml" target="_blank">CLICK TO REGISTER</a>
+								<!-- <a class="site-btn2" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeFQKkX2hk6Q7-QDKlkvYYVWHBbpixeWNSA9aHxJ65TGjR__w/viewform">CLICK TO REGISTER</a> -->
+							<?php
+							}
 
 							?>
 						</div>
@@ -123,42 +106,175 @@
 				</div>
 				<div class="col-lg-6 col-md-6 p-0">
 					<div class="feature-item set-bg" data-setbg="img/ml-pubg/pubg.jpg">
-						<span class="cata new">REGISTER FOR PUBG MOBILE </span>
+						<span class="cata new">PUBG MOBILE </span>
 						<div class="fi-content text-white">
 							<h5>Tournament PUB Mobile of Bayan</h5>
 							<?php
-								$dateTime=date("d-m-Y");
-								
+							$dateTime = date("d-m-Y");
 
-								if ($dateTime=="30-03-2020") {
-									?>
-										<button class="btn btn-disabled">Pendaftaran Ditutup</button>
-									<?php
-								}
-								else{
-									?>
-										<a class="site-btn2" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdUMAnMf_SnaAQnZTot7tvqc7j_b-fMZmvbVVMOFZQ3yBYlQg/viewform">CLICK TO REGISTER</a>
-									<?php
-								}
+
+							if ($dateTime == "30-03-2020") {
+							?>
+								<button class="btn btn-disabled">Pendaftaran Ditutup</button>
+							<?php
+							} else {
+							?>
+								<a href="https://docs.google.com/forms/d/e/1FAIpQLSdUMAnMf_SnaAQnZTot7tvqc7j_b-fMZmvbVVMOFZQ3yBYlQg/viewform" class="regis-pubg" target="_blank">CLICK TO REGISTER</a>
+
+								<!-- <a class="site-btn2" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdUMAnMf_SnaAQnZTot7tvqc7j_b-fMZmvbVVMOFZQ3yBYlQg/viewform">CLICK TO REGISTER</a> -->
+							<?php
+							}
 
 							?>
 						</div>
 					</div>
 				</div>
-				
+
+			</div>
+		</div>
+	</section>
+	<section class="mosh-call-to-action-area bg-img bg-overlay section_padding_100" style="background-image: url(img/ml-pubg/bgfull.png);">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="cta-content text-center wow fadeIn" data-wow-delay="0.5s">
+						<div class="section-heading">
+							<p>INFROMSI LEBIH LANJUT</p>
+							<p>ada pertanyaan untuk GAME dan PEMBAYARAN ?</p>
+							<span class="qna">hubungi Contak di bawah</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-12 mt-3">
+					<div class="row">
+						<div class="col-sm-12 col-md-6 col-lg-6 mt-1">
+							<div class="col-md-12 col-sm-12 text-center text-white">
+								<img src="img/panitia/medi.jpeg" width="80px" class="rounded-circle" alt="">
+								<h5 class="mt-2">medicare</h5>
+								<p>Pertanyaan</p>
+								<a href="https://api.whatsapp.com/send?phone=6281907872481" target="_blank" class="wa">Click to chat via WA</a>
+							</div>
+						</div>
+						<div class="col-sm-12 col-md-6 col-lg-6 mt-1">
+							<div class="col-md-12 col-sm-12 text-center text-white">
+								<img src="img/panitia/riadah.jpg" width="80px" class="rounded-circle" alt="">
+								<h5 class="mt-2">riadah</h5>
+								<p>pembayaran</p>
+								<a href="https://api.whatsapp.com/send?phone=6287785523853" target="_blank" class="wa">Click to chat via WA</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 mt-5">
+					<div class="cta-content text-center wow fadeIn" data-wow-delay="0.5s">
+						<div class="section-heading">
+							<a href="" class="qna2">CLICK untuk Melihat Ketentuan dan syarat tournament</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
 	<!-- Feature section end -->
+	<!-- ***** Team Area Start ***** -->
+	<section class="mt-3">
+		<div class="container">
+			<div class="col-12 col-md-12">
+				<div class="section-heading text-center">
+					<p>PANITIA</p>
+					<h4>DAFTAR KEPANITIAAN
+					</h4>
+				</div>
+			</div>
+			<div class="col-sm-12 col-lg-12 col-md-12 mb-3">
+				<div class="row text-center">
+					<div class="panitia">
+						<img src="img/panitia/medi.jpeg" data-toggle="modal" data-target="#exampleModal" width="70px" height="70px" class="rounded-circle " alt="">
+					</div>
+					<div class="panitia">
+						<img src="img/panitia/sudiana.jpg" data-toggle="modal" data-target="#sudi" width="70px" height="70px" class="rounded-circle " alt="">
+					</div>
+					<div class="panitia">
+						<img src="img/panitia/riadus.jpg" data-toggle="modal" data-target="#riadus" width="70px" height="70px" class="rounded-circle " alt="riadus">
+					</div>
+					<div class="panitia">
+						<img src="img/panitia/rianto.jpg" data-toggle="modal" data-target="#rianto" width="70px" height="70px" class="rounded-circle " alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- medicare -->
+	<div class="modal fade " id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content text-center">
+				<div class="modal-body">
+					<img src="img/panitia/medi.jpeg" width="150px" class="rounded-circle" srcset="">
+					<h5>Medicare</h5>
+					<h6>Laki-laki</h6>
+					<span><i class="fa fa-facebook"></i> medicare</span>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- riadus -->
+	<div class="modal fade " id="riadus" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content text-center">
+				<div class="modal-body">
+					<img src="img/panitia/riadus.jpg" width="150px" class="rounded-circle" srcset="">
+					<h5>riadus</h5>
+					<h6>Laki-laki</h6>
+					<span><i class="fa fa-facebook"></i> riadus</span>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- sudiana -->
+	<div class="modal fade " id="sudi" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content text-center">
+				<div class="modal-body">
+					<img src="img/panitia/sudiana.jpg" width="150px" class="rounded-circle" srcset="">
+					<h5>sudiana</h5>
+					<h6>Laki-laki</h6>
+					<span><i class="fa fa-facebook"></i> sudiana</span>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- rianto -->
+	<div class="modal fade " id="rianto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content text-center">
+				<div class="modal-body">
+					<img src="img/panitia/rianto.jpg" width="150px" class="rounded-circle" srcset="">
+					<h5>rianto</h5>
+					<h6>Laki-laki</h6>
+					<span><i class="fa fa-facebook"></i> rianto</span>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- tomi -->
+	<!-- wardi -->
+
 
 	<!-- Footer section -->
 	<footer class="footer-section">
 		<div class="container">
-			
-			<p class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> Medicare  | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-</p>
+
+			<p class="copyright">
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+				Copyright &copy;<script>
+					document.write(new Date().getFullYear());
+				</script> Medicare | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			</p>
 		</div>
 	</footer>
 	<!-- Footer section end -->
@@ -170,5 +286,11 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> Medic
 	<script src="js/owl.carousel.min.js"></script>
 	<script src="js/jquery.marquee.min.js"></script>
 	<script src="js/main.js"></script>
-    </body>
+	<!-- Popper js -->
+	<script src="js/popper.min.js"></script>
+	<script src="js/plugins.js"></script>
+	<!-- Active js -->
+	<script src="js/active.js"></script>
+</body>
+
 </html>
